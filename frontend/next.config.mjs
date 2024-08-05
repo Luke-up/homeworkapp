22fa2fcea7fca50f4a7cea0ee040e06b3b038561
Luke <painelukeb@gmail.com>
2024-08-05 21:04:47 +0200

    Add file watching to frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    // Set file watching to enable hot reloading in Docker
+    // Not perfect, but it's all we have for now
+    webpack(config, { dev }) {
+      if (dev) {
+        config.watchOptions = {
+          poll: 1000,
+          aggregateTimeout: 300,
+        };
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
