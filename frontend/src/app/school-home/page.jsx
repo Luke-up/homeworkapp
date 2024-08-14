9eec61e850d7f91355818bd1a8afb7019e174ef4
Luke <painelukeb@gmail.com>
2024-08-14 21:29:13 +0200

    Added styles

--- frontend/src/app/school-home/page.jsx
+++ frontend/src/app/school-home/page.jsx
@@ -6,6 +6,8 @@ import axiosInterceptor from '@/utils/axiosInterceptor';
 
 const ClassesPage = () => {
   const [classes, setClasses] = useState([{"name":"No classes found", "id":0}]);
+  const [teachers, setTeachers] = useState([{"name":"No teachers found", "id":0}]);
+  const [students, setStudents] = useState([{"name":"No students found", "id":0}]);
 
   useEffect(() => {
     const fetchClasses = async () => {
@@ -15,6 +17,8 @@ const ClassesPage = () => {
         const data = await res.data;
         if (data != '[]') {
           setClasses(data.classes);
+          setTeachers(data.teachers);
+          setStudents(data.students);
           console.log(res);
         }
       } catch (error) {
@@ -28,27 +32,32 @@ const ClassesPage = () => {
     <div className="classes">
       <h1>Classes</h1>
       {classes.map((classItem) => (
-          <div key={classItem.id}>
+          <div className="class-item" key={classItem.id}>
             <div className="class-top">
               <div className="class-name">
               {classItem.name}
               </div>
               <div className='class-teachers'>
                 Teachers: 
-                {classItem.teachers ? classItem.teachers.map((teacher) => (
-                  <div key={teacher.id} className='teacher'>
-                    {teacher.name}
-                  </div>
-                )): null}
+                {classItem.teachers ? teachers.map((teacher) => (
+                  classItem.teachers.includes(teacher.id) && (
+                    <div key={teacher.id} className='teacher'>
+                      {teacher.name}
+                      <div className="delete-teacher">X</div>
+                    </div>
+                  )
+                )) : null}
                 <span>X</span>
               </div>
             </div>
             <div className="class-students">
               <span>Students:</span><span>X</span>
-              {classItem.students ? classItem.students.map((student) => (
+              {classItem.students ? students.map((student) => (
+                classItem.students.includes(student.id) && (
                 <div key={student.id} className='student'>
                   {student.name}
                 </div>
+                )
               )): null}
             </div>
           </div>
